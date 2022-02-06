9338684274ee38ab95849ff03e0c00317766c8b6
DivyanshArya-23 <divyansharya20@gmail.com>
2022-02-06 16:03:46 +0530

    commit to redeploy app

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./index.module.scss";
 import Login from "../components/Login";
+
 export default function Home() {
   return (
     <div className={styles.appContainer}>
